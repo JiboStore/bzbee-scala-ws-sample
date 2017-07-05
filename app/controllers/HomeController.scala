fed019f49e2ac7f82a35d5c5ae5bdc5aa4e4da37
Hakim Hauston <hakim.hauston@gmail.com>
2017-07-05 18:29:03 +0800

    Allow not same origin to enable test from https://www.websocket.org/echo.html

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -87,7 +87,8 @@ class HomeController @Inject()(@Named("stocksActor") stocksActor: ActorRef,
    * Returns true if the value of the Origin header contains an acceptable value.
    */
   def originMatches(origin: String): Boolean = {
-    origin.contains("localhost:9000") || origin.contains("localhost:19001")
+//    origin.contains("localhost:9000") || origin.contains("localhost:19001")
+    true
   }
 
   /**
